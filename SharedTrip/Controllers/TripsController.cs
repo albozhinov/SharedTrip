ab13dca503a6edaf05e2f2caee4a01314aea5a43
albozhinov <a.bozhinov90@gmail.com>
2022-02-15 15:53:14 +0200

    Register and Login actions are implemented

---
+++ SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedTrip.Controllers
+{
+using BasicWebServer.Server.Controllers;
+    using BasicWebServer.Server.HTTP;
+
+    public class TripsController : Controller
+    {
+        public TripsController(Request request) 
+            : base(request)
+        {
+        }
+
+        public Response All()
+            => View();
+
+        public Response Add()
+            => View();
+
+        public Response Details()
+            => View();
+    }
+}
---
+++ SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,82 @@
+﻿namespace SharedTrip.Controllers
+{
+    using BasicWebServer.Server.Attributes;
+    using BasicWebServer.Server.Controllers;
+    using BasicWebServer.Server.HTTP;
+    using SharedTrip.Contracts;
+    using SharedTrip.ViewModels;
+    using SharedTrip.ViewModels.Users;
+    using System;
+    using System.Collections.Generic;
+
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UsersController(
+            Request request,
+            IUserService _userService) 
+            : base(request)
+        {
+            userService = _userService;
+        }
+
+        [HttpPost]
+        public Response Login(LoginViewModel model)
+        {
+            Request.Session.Clear();
+
+            (string userId, bool isCorrect) = userService.IsLoginCorrect(model);
+
+            if (isCorrect)
+            {
+                SignIn(userId);
+
+                CookieCollection cookies = new CookieCollection();
+                cookies.Add(Session.SessionCookieName,
+                    Request.Session.Id);
+
+                return Redirect("/Trips/All");
+            }
+
+            return View(new List<ErrorViewModel>() { new ErrorViewModel("Login incorrect") }, "/Error");
+        }
+
+        [HttpGet]
+        public Response Login()
+            => View();
+
+        public Response Register()
+            => View();
+
+        [HttpPost]
+        public Response Register(RegisterViewModel model)
+        {
+            var (isValid, errors) = userService.ValidateModel(model);
+
+            if (!isValid)
+            {
+                return View(errors, "/Error");
+            }
+
+            try
+            {
+                userService.RegisterUser(model);
+            }
+            catch (ArgumentException aex)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel(aex.Message) }, "/Error");
+            }
+            catch (Exception)
+            {
+
+                return View(new List<ErrorViewModel>() { new ErrorViewModel("Unexpected Error") }, "/Error");
+            }
+
+            return Redirect("/");
+        }
+
+        public Response Logout()
+            => View();
+    }
+}
