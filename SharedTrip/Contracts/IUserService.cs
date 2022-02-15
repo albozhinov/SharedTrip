dc3eec9adb9d688d0a87a63da33947681bab1392
albozhinov <a.bozhinov90@gmail.com>
2022-02-15 15:58:19 +0200

    IsLoginCorrect method added

---
+++ SharedTrip/Contracts/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using SharedTrip.ViewModels;
+using SharedTrip.ViewModels.Users;
+using System.Collections.Generic;
+
+namespace SharedTrip.Contracts
+{
+    public interface IUserService
+    {
+        (bool isValid, IEnumerable<ErrorViewModel> errors) ValidateModel(RegisterViewModel model);
+        void RegisterUser(RegisterViewModel model);
+        (string userId, bool isCorrect) IsLoginCorrect(LoginViewModel model);
+    }
+}
